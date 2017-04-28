343410b0ed46f8a175da3e9ac0388e4247e7616e
Chadwick Boulay <chadwick.boulay@gmail.com>
2017-04-28 17:52:55 -0400

    scanForDevices() works now, but setDevice crashes without possibility of debugging. I'm giving up on the SWIG wrapper.

--- cerestim/cerestim.i
+++ cerestim/cerestim.i
@@ -4,10 +4,11 @@
  #include "BStimulator.h"
  %}
  #define STATIC_BSTIM_LINK
+ %include "windows.i"
  %include "std_vector.i"
  %include "typemaps.i"
- %template() std::vector<int>;
- %apply std::vector<int> *OUTPUT { std::vector<UINT32> & device_serial_nums };
+ %template() std::vector<UINT32>; 
+ %apply std::vector<UINT32> *OUTPUT { std::vector<UINT32> & device_serial_nums };
  
  /* Parse the header file to generate wrappers */
  %include "BStimulator.h"
--- tests/api_example.py
+++ tests/api_example.py
@@ -3,10 +3,12 @@ This script is intended to follow the example provided with the API in simpleExa
 """
 import cerestim
 
-cerestim.BStimulator.scanForDevices()
-#stimulator = cerestim.BStimulator()
-#usbParams = cerestim.BUsbParams()
-#usbParams.timeout = 1000 # msec
-#usbParams.pid = cerestim.PN7655
-#usbParams.vid
-#result = stimulator.connect(cerestim.BINTERFACE_DEFAULT, usbParams)
+result, device_tuple = cerestim.BStimulator.scanForDevices()  # result = 0, device_tuple = (15623,)
+if len(device_tuple) > 0:
+    stimulator = cerestim.BStimulator()
+    result = stimulator.setDevice(device_tuple[0])
+    # usbParams = cerestim.BUsbParams()
+    # usbParams.timeout = 1000  # msec
+    # usbParams.pid = cerestim.PN7655
+    # usbParams.vid
+    # result = stimulator.connect(cerestim.BINTERFACE_DEFAULT, usbParams)
